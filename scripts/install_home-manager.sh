eecf093c40acb4d0e92c00e22386aba47e90f221
tyasheliy <egorgerasimov@tuta.io>
2024-11-29 22:33:35 +0300

    init

---
+++ scripts/install_home-manager.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e 
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
---
+++ scripts/install_nix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+sh <(curl -L https://nixos.org/nix/install) --daemon
