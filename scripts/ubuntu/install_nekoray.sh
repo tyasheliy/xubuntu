101ab341bee518bf5ae36ca1a669e1dc4823a040
tyasheliy <egorgerasimov@tuta.io>
2024-12-01 20:55:19 +0000

    nekoray cfg fix

--- scripts/ubuntu/install_nekoray.sh
+++ scripts/ubuntu/install_nekoray.sh
@@ -13,4 +13,13 @@ sudo apt install build-essential \
 
 wget -qO- https://raw.githubusercontent.com/ohmydevops/nekoray-installer/main/installer.sh | bash
 
+cfgDir=$HOME/.config/nekoray/config
+
+
+set +e
+mkdir -p $cfgDir
+set -e
+
+ln -s $cfgDir $HOME/NekoRay/nekoray/config
+
 sudo -E $HOME/NekoRay/nekoray/nekoray
