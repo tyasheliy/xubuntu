7b9f7e91e84d1bd9bf3f25203bd757ccc1b8419e
tyasheliy <egorgerasimov@tuta.io>
2025-03-18 22:57:47 +0300

    no sandbox flag for cursor de

--- home/cursor/default.nix
+++ home/cursor/default.nix
@@ -3,7 +3,7 @@
       cursor = {
         name = "Cursor";
         genericName = "IDE";
-        exec = "${userConfig.homeDir}/Applications/cursor.AppImage";
+        exec = "${userConfig.homeDir}/Applications/cursor.AppImage --no-sandbox";
         terminal = false;
       };
   };
