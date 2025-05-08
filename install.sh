d3b95f2950e5f606a709dba620bb80088f8e6fd2
tyasheliy <egorgerasimov@tuta.io>
2025-05-08 04:09:32 +0300

    raw install script

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+if [[ "${EUID}" = 0 ]]; then
+	echo "Won't run it as root."
+	exit 1
+fi
+
+if [[ -z "${USER}" ]]; then
+	exit 1
+fi
+
+if [[ -z "${HOME}" ]]; then
+	exit 1
+fi
+
+installIfNotFound() {
+	command -v $1 >/dev/null 2>&1 || sudo apt install -y $1
+}
+
+rn() {
+	bash $cfgDir/rn.sh $1
+}
+
+hsw() {
+	export NIXPKGS_ALLOW_UNFREE=1 && home-manager switch --flake $cfgDir
+}
+
+replaceDefaultUser() {
+	sed -i "s/tyasheliy/${USER}/g" $1
+}
+
+installIfNotFound git
+
+git clone https://github.com/tyasheliy/xubuntu $HOME/os
+
+cfgDir=$(realpath $HOME/os)
+export SCRIPT_DIR="${cfgDir}/scripts"
+
+set -eEuo pipefail
+
+rn install_nix.sh
+replaceDefaultUser $cfgDir/flake.nix
+replaceDefaultUser $cfgDir/config.nix
+rn install_home-manager.sh
+hsw
+rn set_zsh.sh
